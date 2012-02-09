34b07694d35ce8cdc626fa842f5833c8e9477c97
Felix Lauer <felix.lauer@uni-weimar.de>
2012-02-09 12:48:12 +0100

    built in functionality to add some help text to parameters and set an information trigger to have it print automatically

--- main.cpp
+++ main.cpp
@@ -3,11 +3,12 @@
 int main(int argc, const char** argv) {
 
     sclap::Parser parser;
-    parser.add_argument("--help");
-    parser.parse_arguments(argc, argv);
+    parser.add_parameter("--multisample", 1);
+    parser.set_parameter_info("--multisample", "The multisampling state. Accepts values 4, 9, 16 and 42.");
 
-    if (parser.is_set("--help"))
-        std::cout<<parser.get_value_of<int>("--help")<<std::endl;
+    parser.set_print_info_trigger("--help");
+
+    parser.parse_parameters(argc, argv);
 
     return 0;
 }
--- sclap.hpp
+++ sclap.hpp
@@ -11,65 +11,82 @@ namespace sclap {
 class Parser {
     public:
         Parser():
-            values_(){}
+            values_(),
+            parameter_info_(),
+            print_info_trigger_() {}
 
-        void parse_arguments(int argc, const char** argv) {
+        void parse_parameters(int argc, const char** argv) {
             values_["program_name"].first = true;
             values_["program_name"].second.push_back(argv[0]);
 
             for (int i(1); i<argc; ++i) {
-                std::map<std::string, std::pair<bool, std::vector<std::string> > >::iterator match(values_.find(argv[i]));
-                if (match != values_.end()) {
-                    for (unsigned j(0); j < match->second.second.size(); ++j) {
-                        if (++i < argc) {
-                            if (values_.find(argv[i]) != values_.end()) {
-                                std::cout << "ERROR: Number of values given for argument \""<< match->first << "\" is less than expected! Cancelling..."<<std::endl;
+                if (argv[i] == print_info_trigger_) {
+                    print_parameter_information();
+                    break;
+                } else {
+                    std::map<std::string, std::pair<bool, std::vector<std::string> > >::iterator match(values_.find(argv[i]));
+                    if (match != values_.end()) {
+                        for (unsigned j(0); j < match->second.second.size(); ++j) {
+                            if (++i < argc) {
+                                if (values_.find(argv[i]) != values_.end()) {
+                                    std::cout << "ERROR: Number of values given for parameter \""<< match->first << "\" is less than expected! Cancelling..."<<std::endl;
+                                    return;
+                                }
+                                match->second.second[j] = argv[i];
+
+                            } else if (j < match->second.second.size()){
+                                std::cout << "ERROR: Number of values given for parameter \""<< match->first << "\" is less than expected! Cancelling..."<<std::endl;
                                 return;
                             }
-                            match->second.second[j] = argv[i];
-
-                        } else if (j < match->second.second.size()){
-                            std::cout << "ERROR: Number of values given for argument \""<< match->first << "\" is less than expected! Cancelling..."<<std::endl;
-                            return;
                         }
+                        match->second.first = true;
+                    } else {
+                        std::cout << "ERROR: parameter \""<< argv[i] << "\" is not a valid parameter!"<<std::endl;
                     }
-                    match->second.first = true;
-                } else {
-                    std::cout << "ERROR: Argument \""<< argv[i] << "\" is not a valid parameter!"<<std::endl;
                 }
             }
         }
 
-        void add_argument(std::string const& argument_name, unsigned value_count = 1) {
-            if (values_.find(argument_name) == values_.end())
-                values_.insert(std::make_pair(argument_name, std::make_pair(false, std::vector<std::string>(value_count))));
+        void add_parameter(std::string const& parameter_name, unsigned value_count = 1) {
+            if (values_.find(parameter_name) == values_.end())
+                values_.insert(std::make_pair(parameter_name, std::make_pair(false, std::vector<std::string>(value_count))));
             else {
-                std::cout << "WARNING: Argument \""<< argument_name << "\" has already been set! Overwriting..."<<std::endl;
+                std::cout << "WARNING: parameter \""<< parameter_name << "\" has already been set! Overwriting..."<<std::endl;
             }
         }
 
-        bool is_set(std::string const& argument_name) {
-            std::map<std::string, std::pair<bool, std::vector<std::string> > >::const_iterator searched(values_.find(argument_name));
+        void set_parameter_info(std::string const& parameter_name, std::string const& info) {
+            if (values_.find(parameter_name) != values_.end())
+                parameter_info_.insert(std::make_pair(parameter_name, info));
+            else std::cout << "ERROR: parameter \""<< parameter_name << "\" has not been added to the list of possible parameters! Doing nothing..."<<std::endl;
+        }
+
+        void set_print_info_trigger(std::string const& info_trigger) {
+            print_info_trigger_ =  info_trigger;
+        }
+
+        bool is_set(std::string const& parameter_name) {
+            std::map<std::string, std::pair<bool, std::vector<std::string> > >::const_iterator searched(values_.find(parameter_name));
             if (searched != values_.end())
                 return searched->second.first;
 
-            std::cout << "ERROR: Argument \""<< argument_name << "\" has not been added to the list of possible arguments! Returnig default value..."<<std::endl;
+            std::cout << "ERROR: parameter \""<< parameter_name << "\" has not been added to the list of possible parameters! Returnig default value..."<<std::endl;
             return false;
         }
 
         template <typename T>
-        T const get_value_of(std::string const& argument_name) {
+        T const get_value_of(std::string const& parameter_name) {
             T return_value = T();
-            std::vector<T> return_values(get_values_of<T>(argument_name));
+            std::vector<T> return_values(get_values_of<T>(parameter_name));
             if (!return_values.empty())
                 return_value = return_values.front();
             return return_value;
         }
 
         template <typename T>
-        std::vector<T> const get_values_of(std::string const& argument_name) {
+        std::vector<T> const get_values_of(std::string const& parameter_name) {
             std::vector<T> return_values;
-            std::map<std::string, std::pair<bool, std::vector<std::string> > >::const_iterator searched(values_.find(argument_name));
+            std::map<std::string, std::pair<bool, std::vector<std::string> > >::const_iterator searched(values_.find(parameter_name));
 
             if (searched != values_.end()) {
                 if (searched->second.first) {
@@ -80,13 +97,18 @@ class Parser {
                         return_values.push_back(new_value);
                     }
                 } else {
-                    std::cout << "WARNING: Argument \""<< argument_name << "\" has not been set! Returnig default value..."<<std::endl;
+                    std::cout << "WARNING: parameter \""<< parameter_name << "\" has not been set! Returnig default value..."<<std::endl;
                 }
             } else {
-                std::cout << "ERROR: Argument \""<< argument_name << "\" has not been added to the list of possible arguments! Returnig default value..."<<std::endl;
+                std::cout << "ERROR: parameter \""<< parameter_name << "\" has not been added to the list of possible parameters! Returnig default value..."<<std::endl;
             }
 
             return return_values;
+        }
+
+        void print_parameter_information() const {
+            for (std::map<std::string, std::string>::const_iterator it(parameter_info_.begin()); it!=parameter_info_.end(); ++it)
+                std::cout<<it->first << ": " << it->second << std::cout<<std::endl;
         }
 
         void print_values() const {
@@ -100,6 +122,8 @@ class Parser {
 
     private:
         std::map<std::string, std::pair<bool, std::vector<std::string> > > values_;
+        std::map<std::string, std::string> parameter_info_;
+        std::string print_info_trigger_;
 };
 
 }
